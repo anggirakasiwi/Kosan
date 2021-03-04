5071d04ecb5eeaf643cf3c0baab62ff160224cac
Windows 10 <elfiramputri23@gmail.com>
2021-03-04 22:48:31 +0700

    menu help

--- app/src/main/java/com/example/e_kostan/Menu/Menu_Help.java
+++ app/src/main/java/com/example/e_kostan/Menu/Menu_Help.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 import com.example.e_kostan.R;
 
 public class Menu_Help extends AppCompatActivity {
-
+TextView Help;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu__help);
+        Help=(TextView)findViewById(R.id.help);
+        Help.setText(Html.fromHtml("<i>elfiramputri23@gmail.com</i> <br><i>elfiramputri23@gmail.com</i>"));
     }
     @Override
     public void onBackPressed(){
